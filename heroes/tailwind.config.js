f597a5108722c28a12c0d72d63664fdbb157ea96
Angel <06david078@gmail.com>
2024-03-31 18:31:29 -0500

    Se añade navbar, logo y titulo heroes

---
+++ heroes/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    '../src/app/app.component.html',
+    '../src/app/components/navbar/navbar.component.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
