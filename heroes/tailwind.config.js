eeea20a01f38ac5d1fdd68bb4e8c31902132c016
Angel <06david078@gmail.com>
2024-03-31 20:02:35 -0500

    Primeras 4 cartas

--- heroes/tailwind.config.js
+++ heroes/tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
     '../src/app/app.component.html',
-    '../src/app/components/navbar/navbar.component.html'
+    '../src/app/components/navbar/navbar.component.html',
+    '../src/app/components/biografia/biografia.component.html',
   ],
   theme: {
     extend: {},
